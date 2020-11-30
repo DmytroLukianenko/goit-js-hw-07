cfd95498e475c1bac9af893147437a5d6d0c294d
DmytroLukianenko <71637913+DmytroLukianenko@users.noreply.github.com>
2020-11-30 12:17:01 +0100

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -3,10 +3,10 @@ const btnIncrease = document.querySelector('button[data-action="increment"]')
 const btnDecrease = document.querySelector('button[data-action="decrement"]')
 
 const increment = () => {
-    counterValue.textContent = Number(counterValue.textContent) + Number(btnIncrease.textContent);
+    counterValue.textContent = Number(counterValue.textContent) + 1;
 }
 const decrement = () => {
-    counterValue.textContent = Number(counterValue.textContent) + Number(btnDecrease.textContent);
+    counterValue.textContent = Number(counterValue.textContent) - 1;
 }
 
 
